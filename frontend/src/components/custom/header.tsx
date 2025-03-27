f328a600beb1b18ae3f18d3aa9b67ed15efdf6de
FiBBRLZ <fibbrlz@gmail.com>
2025-03-27 20:28:37 +0100

    added user name to the header

--- frontend/src/components/custom/header.tsx
+++ frontend/src/components/custom/header.tsx
@@ -19,21 +19,39 @@ interface HeaderPops {
     }
 }
 
+interface AuthUserProps {
+    username: string;
+    email: string;
+}
+
+export function LoggedInUser({userData}: { userData: AuthUserProps}) {
+    return (
+        <div className="flex gap-5">
+            <Link href="/dashboard/account" className="font-bold text-m px-3 hover:text-blue-600">{userData.username}</Link>
+            <LogoutButton />
+        </div>
+    );
+}
+
 
 export default async function Header({ data }:  { data : HeaderPops }) {
-    //assign user.ok key to user variable to check if user is logged in
-    const { ok: user } = await getUserMeLoader();
-
+    const user = await getUserMeLoader();
     const header = data;
+
+
     return (
         <header className="py-[20px] bg-slate-50 shadow-lg w-full relative z-20">
             <div className="container">
                 <div className="flex justify-between items-center">
                     <Link className="text-xl font-bold text-black" href={header.logoText.linkUrl}>{header.logoText.linkText}</Link>
                     <nav className="flex gap-5 justify-center items-center">
-                        <Link href="/dashboard">Dashboard</Link>
-                        {user ? (
-                            <LogoutButton />  
+                        <ul className="font-sans text-sm mr-[20px]">
+                            <Link href="/dashboard">Dashboard</Link>
+                        </ul>
+                        {user.ok ? (
+                            <div className="flex gap-5 justify-center items-center px-5 py-3 border border-gray-200 rounded-lg">
+                                <LoggedInUser userData={user.data} />
+                            </div>
                         ): (
                             <Button asChild size={"lg"} variant={'default'}>
                                 <Link href={header.ctaButton.linkUrl}>{header.ctaButton.linkText}</Link>
