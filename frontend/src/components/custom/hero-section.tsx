a5fa18a00026d36fed8023333c80d247b9572b6f
FiBBRLZ <fibbrlz@gmail.com>
2025-03-27 20:31:15 +0100

    changed hero button when signed in

--- frontend/src/components/custom/hero-section.tsx
+++ frontend/src/components/custom/hero-section.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import { StrapiImage } from '@/components/custom/strapi-image';
+import { getUserMeLoader } from "@/data/services/get-user-me-loader";
 
 interface Image {
     url: string;
@@ -27,9 +28,11 @@ interface HeroSectionProps {
 
 
 
-export default function HeroSection( { data }: { readonly data: HeroSectionProps } ) {
+export default async function HeroSection( { data }: { readonly data: HeroSectionProps } ) {
     const image = data.heroImage;
     const link = data.Link;
+    const user = await getUserMeLoader();
+
     return (
         <div className="w-full flex justify-center align-center py-[200px] relative">
             <div className="bg-image absolute inset-0 z-0">
@@ -38,9 +41,15 @@ export default function HeroSection( { data }: { readonly data: HeroSectionProps
             <div className="container text-center relative z-10 flex justify-center items-center flex-col">
                 <h1 className="text-6xl font-black text-white">{data.heroTitle}</h1>
                 <p className="text-3xl my-5 text-white">{data.heroDesc}</p>
-                <Button asChild size={"lg"} variant={'outline'}>
-                    <Link href={link.linkUrl}>{link.linkText}</Link>
-                </Button>
+                {user.ok ? (
+                    <Button asChild size={"lg"} variant={'outline'}>
+                        <Link href="/dashboard">Dashboard</Link>
+                    </Button>
+                ) : (
+                    <Button asChild size={"lg"} variant={'outline'}>
+                        <Link href={link.linkUrl}>{link.linkText}</Link>
+                    </Button>
+                )}
             </div>
         </div>
     );
